ff99ee965b28e893c9651600b10eafcaa76af865
The_Fireplace <the.f1repl4ce@gmail.com>
2019-08-23 12:08:26 -0500

    Fix mixin

--- src/main/java/the_fireplace/chatcensor/sponge/mixin/MixinNetHandlerPlayServer.java
+++ src/main/java/the_fireplace/chatcensor/sponge/mixin/MixinNetHandlerPlayServer.java
@@ -1,7 +1,9 @@
 package the_fireplace.chatcensor.sponge.mixin;
 
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.network.NetHandlerPlayServer;
+import net.minecraft.network.Packet;
 import net.minecraft.network.play.server.SPacketChat;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -16,8 +18,8 @@ public class MixinNetHandlerPlayServer {
     @Shadow
     public EntityPlayerMP player;
 
-    @Inject(method="sendPacket", at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/entity/player/EntityPlayerMP;getChatVisibility()Lnet/minecraft/entity/player/EntityPlayer$EnumChatVisibility;", ordinal = 0), locals = LocalCapture.CAPTURE_FAILSOFT, constraints = "USECHATCENSORMIXIN(1)")
-    public void onSendPacket(SPacketChat spacketchat, CallbackInfo ci) {
+    @Inject(method="sendPacket", at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/entity/player/EntityPlayerMP;getChatVisibility()Lnet/minecraft/entity/player/EntityPlayer$EnumChatVisibility;", ordinal = 0), locals = LocalCapture.CAPTURE_FAILSOFT/*, constraints = "USECHATCENSORMIXIN(1)"*/)
+    public void onSendPacket(final Packet<?> packetIn, CallbackInfo ci, SPacketChat spacketchat, EntityPlayer.EnumChatVisibility entityplayer$enumchatvisibility) {
         spacketchat = the_fireplace.chatcensor.util.NetworkUtils.createModifiedChat(player, spacketchat);
     }
 }
